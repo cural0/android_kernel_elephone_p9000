5f524795734608a0909d27305573e6d8aa83319a
cural0 <michal.cural@gmail.com>
2016-10-17 20:18:40 +0200

    "Update makekernel.sh to use 12 cores"

--- makekernel.sh
+++ makekernel.sh
@@ -10,6 +10,6 @@ fi
 make clean && make mrproper
 export CROSS_COMPILE=~/android/android-ndk-r12b/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin/aarch64-linux-android-
 export ARCH=arm64
-make -j8 $DEFCONFIG
-make -j8
+make -j12 $DEFCONFIG
+make -j12
 cp arch/arm64/boot/Image.gz-dtb $KERNEL
